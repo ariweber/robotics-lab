eba4ad55ce18520339571c87379d78482e55a18f
aharon weber <a.w770770@gmail.com>
2026-07-28 11:57:27 +0300

    ×connect to db mongodb and supabase

---
+++ src/DB/mongoDB.js
@@ -0,0 +1,15 @@
+import {MongoClient} from "mongodb"
+import "dotenv/config"
+
+
+const client = new MongoClient(process.env.MONGO_URL);
+
+
+try {
+  await client.connect()
+  console.log("mongo connected")
+} catch (error) {
+  console.log(error.message)
+}
+
+export const db = client.db("users")
--- src/DB/supabase.js
+++ src/DB/supabase.js
@@ -1,1 +1,11 @@
-i
+import { createClient } from '@supabase/supabase-js'
+import "dotenv/config"
+
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+)
+
+const reslut = await supabase.from("sessions").select()
+console.log(reslut.data)
