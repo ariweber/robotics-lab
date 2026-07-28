8877fcc3d1d529ebdef069bdd3527920a6d7fb07
aharon weber <a.w770770@gmail.com>
2026-07-28 14:21:18 +0300

    add routes

---
+++ src/routes/users.router.js
@@ -0,0 +1,15 @@
+import Router from 'express';
+
+const router = Router()
+
+router.get("/:usersID", async (req, res) => {
+  res.json({})
+
+})
+
+
+router.post("/", async (req, res) => {
+  res.json({})
+})
+
+export default router
